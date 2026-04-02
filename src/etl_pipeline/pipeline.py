c80f3e571864516557b8b939c82f8cbbffb8b6e3
unisom0rphic <117302827+unisom0rphic@users.noreply.github.com>
2026-04-02 08:52:59 +0500

    refactor: pass spark to etl_pipeline() as a dependency

--- src/etl_pipeline/pipeline.py
+++ src/etl_pipeline/pipeline.py
@@ -1,11 +1,9 @@
 from src.etl_pipeline.bronze import bronze_layer
 from src.etl_pipeline.gold import gold_layer
 from src.etl_pipeline.silver import silver_layer
-from src.etl_pipeline.spark_config import get_spark_session
 
 
-def etl_pipeline(table_name):
-    spark = get_spark_session()
+def etl_pipeline(table_name, spark):
     warehouse_path = spark.conf.get("spark.sql.warehouse.dir")
     bronze_rows = bronze_layer(
         # TODO: change source path to a variable later (or kafka topic)
@@ -27,6 +25,3 @@ def etl_pipeline(table_name):
     assert silver_rows <= gold_rows
     # TODO: tests
     # TODO: vectorize features
-
-    # FIXME: stops the context and it dies!
-    # spark.stop()
